5d3524b18f5adfc89b851e0e745bc82b84a11f70
Mahmodian <mahmodian@SAA.LOCAL>
2024-06-05 15:05:53 +0330

    here is an example

--- first_TEST_GIT.cpp
+++ first_TEST_GIT.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    cout<< "hello world"<<endl;
+int main()
+{
+    cout << "hello world" << endl;
+    cout << "amir hosein pesar khobist" << endl;
 }
